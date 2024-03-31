ed17162ca14e2ea6df1e5e0d6d5814cd646256cc
minji-hub <alswlqkr1212@naver.com>
2024-04-01 02:57:33 +0900

    민지

---
+++ minji/src/Main.java
@@ -0,0 +1,55 @@
+//import java.util.Random;
+//import java.util.Scanner;
+//
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(10);
+//        int input = 0;
+//        int count = 0;
+//        do {
+//            System.out.printf("0~9까지의 숫자를 입력하세요:");
+//            input = sc.nextInt();
+//            count++;
+//            if (input != randomNumber) {
+//                System.out.println("땡! 틀렸습니다.");
+//            }
+//        }while (input!=randomNumber);
+//        System.out.printf("%d번만에 맞추셨습니다! 정답: %d",count,randomNumber);
+//        }
+//    }
+
+
+
+
+
+
+
+
+
+
+
+//import org.w3c.dom.ls.LSOutput;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) {
+////        for(int i = 1; i<=9; i++) {
+////            for (int j = 1; j <= 9; j++) {
+////                System.out.printf("%d * %d = %02d  ", j, i, j * i);
+////            }
+////            System.out.println();
+////        }
+//        int dan = 1;
+//        while(dan<10){
+//            int index = 1;
+//            while(index<10) {
+//                System.out.printf("%d * %d = %02d ",index,dan,index * dan);
+//                index++;
+//                System.out.println();
+//                dan++;
+
+
