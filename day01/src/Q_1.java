7579dfd8c048bb59b8dbc17fdd8a82872922e328
정두두 <112332792+JeongBuBu@users.noreply.github.com>
2024-04-03 18:22:10 +0900

    과제 제출

--- day01/src/Q_1.java
+++ day01/src/Q_1.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class Q_1 {
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Q_1 {
+/*           * Q.다음 중 프로그래밍 언어가 아닌 것은?
+            * 1. 자바
+       * 2. 파이썬
+       * 3. C언어
+       * 4. 멋쟁이사자처럼
+       *
+               * 삼항 연산자와 대화상자를 사용해서 퀴즈 프로그램 만들기
+       * 잘못 입력하였을 때에는 '?'를 출력한다.
+            * 정답이면 "정답" 출력
+       * 오답이면 "오답" 출력
+       **/
+public static void main(String[] args) {
+    // Scanner 객체 생성
+    Scanner scanner = new Scanner(System.in);
+
+    // 문제와 선택지, 정답 설정
+    String qMsg = "Q.다음 중 프로그래밍 언어가 아닌 것은?";
+    String choiceMsg = "1. 자바\n" +
+            "2. 파이썬\n" +
+            "3. C언어\n" +
+            "4. 멋쟁이사자처럼";
+    int answer = 4;
+    String result;
+
+    boolean isTrue = false;
+
+    // 정답이 나올 때까지 반복
+    do {
+        // 문제 출력
+        System.out.println(qMsg + "\n" + choiceMsg);
+
+        // 사용자 입력이 정수가 아닐 경우 반복하여 올바른 입력을 기다림
+        while (!scanner.hasNextInt()) {
+            System.out.println("올바른 숫자를 입력하세요.");
+            scanner.next(); // 잘못된 입력을 버립니다.
+        }
+
+        // 정수 입력 받음
+        int choice = scanner.nextInt();
+
+        // 입력값이 정답인지 확인
+        isTrue = choice == answer;
+
+        // 결과 출력
+        //삼항연산자 선언
+        result = isTrue ? "정답" : (choice >= 1 && choice <= 4 ? "오답" : "?");
+        System.out.println(result);
+    } while (!isTrue); // 정답이 나올 때까지 반복
+}
 }
--- day01/src/Q_2.java
+++ day01/src/Q_2.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Q_2 {
+import java.util.Scanner;
+
+public class Q_2 {
+    public static void main(String[] args) {
+
+        // Scanner 객체 생성
+        Scanner sc = new Scanner(System.in);
+
+        // 사용자로부터 숫자 입력 받음
+        System.out.print("숫자 : ");
+        int input = sc.nextInt();
+        System.out.println();
+
+        // 입력 받은 숫자에 해당하는 구구단 제목 출력
+        System.out.println("===== " + input + "단 =====");
+        System.out.println();
+
+        // 구구단 계산 및 출력
+        for(int i = 1; i <= 9; i++) {
+            // 입력 받은 숫자와 i를 곱하여 구구단을 계산하고 출력
+            for(int j = input; j <= input; j++) {
+                System.out.printf("%d * %d = %d   ", j, i, j*i);
+            }
+            System.out.println(); // 한 줄 띄기
+        }
+    }
 }
